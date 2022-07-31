12d1b211b3cb237842eaec660c40c19c5bfe2747
sgteryn <sgteryn@gmail.com>
2022-07-31 18:53:22 -0400

    additional commit

--- server/index.js
+++ server/index.js
@@ -16,8 +16,49 @@ console.log(rollbar)
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+const sweets = []
+
 app.get('/', function(req, res) {
+    rollbar.info('user accessed the main page')
     res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+
+app.get('/api/sweets', (req, res) => {
+    rollbar.info('somebody made a sweets list')
+    res.status(200).send(sweets)
+})
+
+app.post('/api/sweets', (req, res) => {
+   let {sweet} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && sweet !== '') {
+           students.push(sweet)
+           rollbar.log(`${sweet} added successfully`)
+           res.status(200).send(sweets)
+       } else if (sweet === ''){
+            rollbar.warning('no sweet item was given')
+           res.status(400).send('Please enter your favorite SWEET TOOTH satisfier.')
+       } else {
+        rollbar.critical('user tried adding a sweet item that already exists')
+           res.status(400).send('That sweet item already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/sweets/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    sweets.splice(targetIndex, 1)
+    rollbar.warning('sweet-tooth satisfier deleted')
+    res.status(200).send(students)
 })
 const port = process.env.PORT || 5501
 
